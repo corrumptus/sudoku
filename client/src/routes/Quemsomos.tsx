fd8c3fe3fccfc11aca91dab0458df0fd2b85dc9f
corrumptus <lazarinilucas42@gmail.com>
2024-05-05 16:53:29 -0300

    creating quem somos layout

--- client/src/routes/Quemsomos.tsx
+++ client/src/routes/Quemsomos.tsx
@@ -1,5 +1,9 @@
 export default function Quemsomos() {
   return (
-    <div>Quemsomos</div>
+    <div className="quemsomos">
+      App desenvolvido por Lucas Lazarini, prontuario SC3030121.
+      Tarefa de Programação para Web IV(PW4),
+      no 1° semestre de 2024 ministrado por Danilo Moschetto.
+    </div>
   )
 }

d12c7d51976bdeae90229b7344bc33f16a1d9da3
corrumptus <lazarinilucas42@gmail.com>
2024-05-05 14:01:06 -0300

    changing the sudoku representation from layout to Jogar

--- client/src/routes/Jogar.tsx
+++ client/src/routes/Jogar.tsx
@@ -1,5 +1,13 @@
+import SudokuRepresentation from "../components/SudokuRepresentation";
+import { getNovoJogo } from "../sudoku-game-API/sudokuAPI";
+
 export default function Jogar() {
   return (
-    <div>Jogar</div>
+    <div className="app">
+      <h1>Sudoku</h1>
+      <SudokuRepresentation
+        posicoes={getNovoJogo()}
+      />
+    </div>
   )
 }

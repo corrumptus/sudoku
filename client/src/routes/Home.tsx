caa6f4138da7cbf0665b15ab6ec48aef1c16c68c
corrumptus <lazarinilucas42@gmail.com>
2024-05-05 16:39:06 -0300

    creating home layout

--- client/src/routes/Home.tsx
+++ client/src/routes/Home.tsx
@@ -1,5 +1,11 @@
+import { Link } from "react-router-dom";
+
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className="home">
+      <h2>Comece a jogar sudoku agora</h2>
+      <p>Sudoku consiste em um tabuleiro 9 por 9 com números de 0 a 9 faltando, porém cada linha, coluna e bloco 3 por 3 não podem possuir números repetidos</p>
+      <Link to="/jogar">Jogar</Link>
+    </div>
   )
 }

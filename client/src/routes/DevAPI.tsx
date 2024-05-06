7abdc1e2d831e0c040e645bef0c04cda3f80e718
corrumptus <lazarinilucas42@gmail.com>
2024-05-06 10:33:04 -0300

    changing the  dev api layout

--- client/src/routes/DevAPI.tsx
+++ client/src/routes/DevAPI.tsx
@@ -1,7 +1,22 @@
+import { useState } from "react"
+import RouterDescription from "../components/api/RouterDescription";
+import backendRoutes from "../../public/backendMap.json"
+
 export default function DevAPI() {
+  const [ route, setRoute ] = useState(0);
+
   return (
     <div className="api">
-      
+      <nav>
+        {backendRoutes.map((routes, i) =>
+          <button key={i} onClick={() => setRoute(i)} className="route_nav_button">
+            {routes.name}
+          </button>
+        )}
+      </nav>
+      <RouterDescription
+        description={backendRoutes[route]}
+      />
     </div>
   )
 }

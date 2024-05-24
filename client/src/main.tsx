64ca35fa789e2964eb04b24aeee16b7644f33c38
corrumptus <lazarinilucas42@gmail.com>
2024-05-24 13:02:36 -0300

    refactoring the code to a browser router provider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
-import router from './Routes'
 import './index.css'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import Home from './routes/Home'
+import Jogar from './routes/Jogar'
+import Quemsomos from './routes/Quemsomos'
+import DevAPI from './routes/DevAPI'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
+    <Router>
+      <nav className="main-navbar">
+        <Link to="/">Home</Link>
+        <Link to="/jogar">Jogar</Link>
+        <Link to="/quemsomos">Quem somos</Link>
+        <Link to="/api">API para devs</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/jogar" element={<Jogar />} />
+        <Route path="/quemsomos" element={<Quemsomos />} />
+        <Route path="/api" element={<DevAPI />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
 )

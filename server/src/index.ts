66816482e3823a2b7cce85966b93d64adc8efb74
corrumptus <lazarinilucas42@gmail.com>
2024-05-27 09:50:18 -0300

    moving the index to src folder and adding cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,5 @@
 import express, { Express, json } from 'express';
+import cors from "cors";
 
 const PORT: number = Number(process.env.PORT) || 5000;
 
@@ -6,4 +7,6 @@ const api: Express = express();
 
 api.listen(PORT, () => console.log(`server starts at port ${PORT}`));
 
-api.use(json())
+api.use(json());
+
+api.use(cors());

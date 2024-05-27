807eb6fb7b1ee1cde7052efc1bfc65395127b551
corrumptus <lazarinilucas42@gmail.com>
2024-05-27 09:53:07 -0300

    creating a login route

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,7 @@
 import express, { Express, json } from 'express';
 import cors from "cors";
+import UserResponse from './utils/UserResponse';
+import UserRepository from './repository/UserRepository';
 
 const PORT: number = Number(process.env.PORT) || 5000;
 
@@ -13,4 +15,29 @@ api.use(cors());
 
 api.get("/", (_, res) => {
     res.redirect("sudoku-pi-umber.vercel.app/api");
+});
+
+api.post("/login", (req, res) => {
+    const { name, password } = req.body;
+
+    if (name === undefined || name.trim() === "") {
+        res.status(400).json(UserResponse.ofError("Name cannot be blank"));
+        return;
+    }
+
+    if (password === undefined || password.trim() === "") {
+        res.status(400).json(UserResponse.ofError("Password cannot be blank"));
+        return;
+    }
+
+    const user = UserRepository.get(name);
+
+    if (user === undefined || password !== user.password) {
+        res.status(404).json(UserResponse.ofError("Username and/or password are incorrect"));
+        return;
+    }
+
+    const token = "";
+
+    res.json(UserResponse.ofToken(token));
 });

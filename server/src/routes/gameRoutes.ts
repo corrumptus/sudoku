5bd689e395d36a37f8bc1a30a11b072c4fbfb86a
corrumptus <lazarinilucas42@gmail.com>
2024-05-30 13:12:41 -0300

    creating a GET /jogos/new route

--- server/src/routes/gameRoutes.ts
+++ server/src/routes/gameRoutes.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import GameResponse from "../utils/GameResponse";
 import GameService from "../service/GameService";
+import TokenService from "../service/TokenService";
 
 const gameRoutes = express();
 
@@ -17,6 +18,31 @@ gameRoutes.get("/jogos", (req, res) => {
     const games = GameService.getAll(pageNumber);
 
     res.json(GameResponse.ofGames(games));
+});
+
+gameRoutes.get("/jogos/new", (req, res) => {
+    const token = req.headers.authorization;
+
+    if (token === undefined) {
+        res.json(GameResponse.ofGame(GameService.getRandom()));
+        return;
+    }
+
+    const payload = TokenService.validate(token);
+
+    if (payload === undefined) {
+        res.status(404).json(GameResponse.ofError("Invalid Token"));
+        return;
+    }
+
+    const newGame = GameService.getNew(payload.name);
+
+    if (newGame === undefined) {
+        res.json(GameResponse.ofError("Player exceed the max amount of game possibilities"));
+        return;
+    }
+
+    res.json(GameResponse.ofGame(newGame));
 });
 
 export default gameRoutes;

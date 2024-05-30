9e2eb156f78b27b2626fe95a04717594652e74c2
corrumptus <lazarinilucas42@gmail.com>
2024-05-30 13:04:00 -0300

    initing the game repository

---
+++ server/src/repository/GameRepository.ts
@@ -0,0 +1,37 @@
+export default class GameRepository {
+    static getAll(page: number): Game[] {
+        return [];
+    }
+
+    static get(id: number): Game | undefined {
+        return undefined;
+    }
+
+    static newTime(id: number, name: string, time: number): Game | undefined {
+        return undefined;
+    }
+
+    static getRanking(id: number): Ranking | undefined {
+        return undefined;
+    }
+
+    static get amount(): number {
+        return -1;
+    }
+}
+
+type LockedCell = {
+    x:0|1|2|3|4|5|6|7|8,
+    y:0|1|2|3|4|5|6|7|8,
+    valor:1|2|3|4|5|6|7|8|9
+}
+
+export type Game = LockedCell[]
+
+export type Ranking = {
+    game: Game;
+    ranking: {
+        player: string;
+        time: number;
+    }
+}

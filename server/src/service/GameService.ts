e766c9e81371839ad9e0f15849c99e1bb7b9bfa0
corrumptus <lazarinilucas42@gmail.com>
2024-05-30 13:07:58 -0300

    initing a get all

--- server/src/service/GameService.ts
+++ server/src/service/GameService.ts
@@ -1,5 +1,7 @@
 import GameRespository, { Game } from "../repository/GameRepository";
 
 export default class GameService {
-    
+    static getAll(page: number): Game[] {
+        return [];
+    }
 }

b6d185f4d308a166bfc933e446d8d01d14fad3b0
corrumptus <lazarinilucas42@gmail.com>
2024-05-30 13:08:37 -0300

    creating a GET /jogos route

--- server/src/routes/gameRoutes.ts
+++ server/src/routes/gameRoutes.ts
@@ -1,5 +1,22 @@
 import express from "express";
+import GameResponse from "../utils/GameResponse";
+import GameService from "../service/GameService";
 
 const gameRoutes = express();
+
+gameRoutes.get("/jogos", (req, res) => {
+    const { page } = req.query;
+
+    const pageNumber = Number(page);
+
+    if (page === undefined || isNaN(pageNumber)) {
+        res.status(400).json(GameResponse.ofError("Page must be a valid number"));
+        return;
+    }
+
+    const games = GameService.getAll(pageNumber);
+
+    res.json(GameResponse.ofGames(games));
+});
 
 export default gameRoutes;

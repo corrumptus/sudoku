b5025895fbb716329c02c2ad790d6c72de368f73
corrumptus <lazarinilucas42@gmail.com>
2024-06-03 17:01:39 -0300

    creating a function that submits the form

--- client/src/routes/Login.tsx
+++ client/src/routes/Login.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, MouseEvent, useState } from "react";
+import { ChangeEvent, FormEvent, MouseEvent, useState } from "react";
 
 export default function Login() {
   const [ login, setLogin ] = useState<{name: string, password: string}>({ name: "", password: "" });
@@ -8,6 +8,29 @@ export default function Login() {
     const newValue = e.target.value;
 
     setLogin(prev => ({...prev, [field]: newValue}));
+  }
+
+  async function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+
+    const response = await fetch("http://localhost:5000/login", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json"
+      },
+      body: JSON.stringify(login)
+    });
+
+    if (!response.ok) {
+      const { error }: { error: string } = await response.json();
+
+      alert(error);
+      return;
+    }
+
+    const { token }: { token: string } = await response.json();
+
+    localStorage.setItem("sudoku-token", token);
   }
 
   function clear(e: MouseEvent<HTMLButtonElement, globalThis.MouseEvent>) {
@@ -21,7 +44,7 @@ export default function Login() {
 
   return (
     <div className="login">
-      <form>
+      <form onSubmit={handleSubmit}>
         <h1>Login</h1>
         <div>
           <label htmlFor="name">Nome</label>

f740be86dbd04428b78525ea5707a9681ad6d953
corrumptus <lazarinilucas42@gmail.com>
2024-06-03 17:46:15 -0300

    creating the layout of the sign up route

--- client/src/routes/Signup.tsx
+++ client/src/routes/Signup.tsx
@@ -1,5 +1,25 @@
 export default function Signup() {
   return (
-    <div>Signup</div>
+    <div>
+      <form>
+        <h1>Entrar</h1>
+        <div>
+          <label htmlFor="name">Nome</label>
+          <input type="text" id="name" />
+        </div>
+        <div>
+          <label htmlFor="password">Senha</label>
+          <input type="text" id="password" />
+        </div>
+        <div>
+          <label htmlFor="confirmPassword">Confirmação de senha</label>
+          <input type="text" id="confirmPassword" />
+        </div>
+        <div>
+          <button>Limpar</button>
+          <button>Entrar</button>
+        </div>
+      </form>
+    </div>
   )
 }

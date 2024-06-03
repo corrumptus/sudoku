53b090e35ea69deffd3be1e0d009ea0268b4d944
corrumptus <lazarinilucas42@gmail.com>
2024-06-03 18:20:12 -0300

    moving the route declarations to a separate file

---
+++ client/src/SudokuRoutes.tsx
@@ -0,0 +1,20 @@
+import { Routes, Route } from "react-router-dom";
+import DevAPI from "./routes/DevAPI";
+import Home from "./routes/Home";
+import Jogar from "./routes/Jogar";
+import Login from "./routes/Login";
+import Quemsomos from "./routes/Quemsomos";
+import Signup from "./routes/Signup";
+
+export default function SudokuRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/jogar" element={<Jogar />} />
+      <Route path="/quemsomos" element={<Quemsomos />} />
+      <Route path="/api" element={<DevAPI />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+    </Routes>
+  )
+}
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,28 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 import ActionBar from './ActionBar'
-import Home from './routes/Home'
-import Jogar from './routes/Jogar'
-import Quemsomos from './routes/Quemsomos'
-import DevAPI from './routes/DevAPI'
-import Login from './routes/Login'
-import Signup from './routes/Signup'
 import './index.css'
 import "./globals.css"
+import SudokuRoutes from './SudokuRoutes'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Router>
       <ActionBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/jogar" element={<Jogar />} />
-        <Route path="/quemsomos" element={<Quemsomos />} />
-        <Route path="/api" element={<DevAPI />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Signup />} />
-      </Routes>
+      <SudokuRoutes />
     </Router>
   </React.StrictMode>
 )

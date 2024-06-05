945d3ec3ce9313d3a1b3a208deaad42d76c4d687
corrumptus <lazarinilucas42@gmail.com>
2024-06-05 19:03:28 -0300

    creating the layout of the non completed game card component

--- client/src/components/user/NonCompletedGameCard.tsx
+++ client/src/components/user/NonCompletedGameCard.tsx
@@ -1,5 +1,16 @@
+import { useNavigate } from "react-router-dom";
+
 export default function NonCompletedGameCard({ game }: { game: number }) {
+  const navigate = useNavigate();
+
   return (
-    <div>NonCompletedGameCard</div>
+    <div
+      className="non-completed-game-card"
+      onClick={() => {
+        navigate("/jogar/" + game);
+      }}
+    >
+      <img src="../../../public/logo.png" alt="sudoku table icon" />
+    </div>
   )
 }

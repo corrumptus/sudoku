997800938597f1c12aeb72f1a6db5f89440c7f75
corrumptus <lazarinilucas42@gmail.com>
2024-06-05 19:17:47 -0300

    creating the layout of the ranking route

--- client/src/routes/Ranking.tsx
+++ client/src/routes/Ranking.tsx
@@ -1,5 +1,31 @@
+import { useParams } from "react-router-dom";
+import useRanking from "../utils/useRanking";
+
 export default function Ranking() {
+  const { id } = useParams();
+
+  const gameRanking = useRanking(Number(id));
+
+  if (gameRanking === undefined) return (
+    <div className="ranking not-found">
+      <h1>User not found</h1>
+    </div>
+  )
+
   return (
-    <div>Ranking</div>
+    <div className="ranking">
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>Colocação</th>
+              <th>Jogador</th>
+              <th>Tempo</th>
+            </tr>
+          </thead>
+          <tbody></tbody>
+        </table>
+      </div>
+    </div>
   )
 }

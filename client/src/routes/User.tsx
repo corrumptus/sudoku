09d5a9c3de9f60956fbf7a8f20a31f0949509c6b
corrumptus <lazarinilucas42@gmail.com>
2024-06-07 22:05:04 -0300

    changing the user route layout

--- client/src/routes/User.tsx
+++ client/src/routes/User.tsx
@@ -11,7 +11,7 @@ export default function User() {
 
   if (userInfos === undefined) return (
     <div className="user not-found">
-      <h1>User not found</h1>
+      <h1>User not found: {name}</h1>
     </div>
   )
 
@@ -22,16 +22,19 @@ export default function User() {
       </div>
       <div className="container">
         <div className="last-times">
+          <h2>Últimos jogos</h2>
           <ul>
             {userInfos.lastTimes.map(lt => <LastTimeCard key={lt.gameID} game={lt.gameID} time={lt.time} />)}
           </ul>
         </div>
         <div className="self-ranking">
+          <h2>Melhores tempos</h2>
           <ul>
             {userInfos.timeRankings.map((t, i) => <SelfRanking key={t+i} index={i} time={t} />)}
           </ul>
         </div>
         <div className="to-complete">
+          <h2>Jogos começados</h2>
           <ul>
             {userInfos.nonFinishedGames.map(g => <NonCompletedGameCard key={g} game={g} />)}
           </ul>

9bec188abb229f428c051526aecf63ffab313687
corrumptus <lazarinilucas42@gmail.com>
2024-06-08 02:54:52 -0300

    adding navigation to the game rank card component

--- client/src/components/ranking/GameRanking.tsx
+++ client/src/components/ranking/GameRanking.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import { Ranking } from "../../utils/useRanking";
 import { numberToTime } from "../../utils/utils";
 
@@ -8,10 +9,12 @@ export default function gameRanking({
   ranking: Ranking,
   index: number
 }) {
+  const navigate = useNavigate();
+
   return (
     <tr className="ranking-card">
       <td>{index+1}</td>
-      <td>{ranking.name}</td>
+      <td onClick={() => navigate("/user/" + ranking.name)}>{ranking.name}</td>
       <td>{numberToTime(ranking.time)}</td>
     </tr>
   )

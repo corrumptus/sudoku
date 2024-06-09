e88cfcaa5fec9d43de8a17d85c79c9313a62d338
corrumptus <lazarinilucas42@gmail.com>
2024-06-08 23:40:37 -0300

    changing the sudoku representation

--- client/src/components/jogar/SudokuRepresentation.tsx
+++ client/src/components/jogar/SudokuRepresentation.tsx
@@ -1,41 +1,75 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import SudokuCell from "./SudokuCell";
 import { verificaJogo } from "../../sudoku-game-API/sudokuAPI";
 
 export type sudokuRange = 0|1|2|3|4|5|6|7|8;
 export type sudokuValue = 1|2|3|4|5|6|7|8|9;
+export type LockedCell = { x: sudokuRange, y: sudokuRange, valor: sudokuValue }
 
-export default function SudokuRepresentation({
-  posicoes
-}: {
-  posicoes: { x: sudokuRange, y: sudokuRange, valor: sudokuValue }[]
-}) {
-  const posicoesTrancadas: { posicao: number, valor: sudokuValue }[] = posicoes.map(
-    ({ x, y, valor }) => ({posicao: x + y*9, valor: valor})
-  );
-  const [ tabela, setTabela ] = useState(generateSudoku());
+export default function SudokuRepresentation({ id }: { id: number }) {
+  const navigate = useNavigate();
+
+  const [ state, setState ] = useState(false);
+  const [ tabela, setTabela ] = useState<(sudokuValue | undefined)[]>();
+  const posicoesTrancadas = useRef<{ posicao: number, valor: sudokuValue }[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      setState(true);
+      const response = await fetch("http://localhost:5000/jogos/" + id);
+
+      if (!response.ok) {
+        const { error } = await response.json();
+
+        alert(error);
+
+        return;
+      }
+
+      const { game: { lockedCells } }: { game: { lockedCells: LockedCell[] } }
+        = await response.json();
+
+      posicoesTrancadas.current = lockedCells.map(
+        ({ x, y, valor }) => ({posicao: x + y*9, valor: valor})
+      );
+
+      setTabela(generateSudoku());
+    })()
+  }, []);
 
   function generateSudoku(): (sudokuValue | undefined)[] {
-
     let tabela: (sudokuValue | undefined)[] = Array.apply(null, Array(81)).map(() => undefined);
 
-    for (let { posicao, valor } of posicoesTrancadas)
+    for (let { posicao, valor } of posicoesTrancadas.current)
       tabela[posicao] = valor;
 
     return tabela;
   }
 
   function posicaoTrancada(posicao: number): boolean {
-    return posicoesTrancadas.find(p => p.posicao === posicao) !== undefined;
+    return posicoesTrancadas.current.find(p => p.posicao === posicao) !== undefined;
   }
 
   function atualizaValor(posicao: number, newValue: sudokuValue | undefined) {
-    let novaTabela = [...tabela];
+    let novaTabela = [...tabela as (sudokuValue | undefined)[]];
 
     novaTabela[posicao] = newValue;
 
     setTabela(novaTabela);
   }
+
+  if (state) return (
+    <div className="sudoku_rapper loading">
+      <h1>Carregando</h1>
+    </div>
+  )
+
+  if (tabela === undefined) return (
+    <div className="sudoku_rapper not-found">
+      <h1>Jogo não encontrado: {id}</h1>
+    </div>
+  )
 
   return (
     <div className="sudoku_rapper">
@@ -50,7 +84,10 @@ export default function SudokuRepresentation({
           />
         )}
       </div>
-      <button onClick={() => verificaJogo(tabela)}>Checar</button>
+      <div className="buttons">
+        <button onClick={() => verificaJogo(tabela)}>Checar</button>
+        <button onClick={() => navigate("/ranking/" + id)}>Ranking</button>
+      </div>
     </div>
   )
 }

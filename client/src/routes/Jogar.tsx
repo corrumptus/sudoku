6eef461943daeafda8f33b36679054b13e98afd1
corrumptus <lazarinilucas42@gmail.com>
2024-06-08 23:40:56 -0300

    changing the jogar route component

--- client/src/routes/Jogar.tsx
+++ client/src/routes/Jogar.tsx
@@ -1,14 +1,16 @@
+import { useParams } from "react-router-dom";
 import SudokuRepresentation from "../components/jogar/SudokuRepresentation";
-import { getNovoJogo } from "../sudoku-game-API/sudokuAPI";
 import "../styles/jogar.css";
 
 export default function Jogar() {
+  const { id } = useParams();
+
+  
+
   return (
     <div className="jogar">
       <h1>Sudoku</h1>
-      <SudokuRepresentation
-        posicoes={getNovoJogo()}
-      />
+      <SudokuRepresentation id={Number(id)} />
     </div>
   )
 }

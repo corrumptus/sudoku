ee9f5657c896adb41a9406ef0a56055097b92d9e
corrumptus <lazarinilucas42@gmail.com>
2024-06-11 20:39:46 -0300

    implementing the get

--- server/src/repository/UserRepository.ts
+++ server/src/repository/UserRepository.ts
@@ -1,6 +1,17 @@
+import User from "../model/User";
+
 export default class UserRepository {
-    static get(name: string): User | undefined {
-        return undefined;
+    static async get(name: string): User | undefined {
+        const user = await User.findOne({
+            where: {
+                name: name
+            }
+        });
+
+        if (user === null)
+            return undefined;
+
+        return user as unknown as User;
     }
 
     static create(name: string, password: string): User | undefined {

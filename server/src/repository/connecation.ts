70d51f4b8d3b9f5c1ae544fc98f28f4ff7735bc2
corrumptus <lazarinilucas42@gmail.com>
2024-06-11 16:10:21 -0300

    implementing the connection

--- server/src/repository/connecation.ts
+++ server/src/repository/connecation.ts
@@ -1,3 +1,14 @@
-const connection = null;
+import { Dialect, Sequelize } from "sequelize";
+
+const connection = new Sequelize(
+    process.env.DATABASE_NAME as string,
+    process.env.DATABASE_USER as string,
+    process.env.DATABASE_PASSWORD as string,
+    {
+        dialect: process.env.DATABASE_DIALECT as Dialect,
+        host: process.env.DATABASE_HOST as string,
+        port: Number(process.env.DATABASE_PORT)
+    }
+);
 
 export default connection;

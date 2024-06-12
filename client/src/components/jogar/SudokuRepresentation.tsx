552e8992009982dc0d022f46225934c1058bd4c0
corrumptus <lazarinilucas42@gmail.com>
2024-06-12 15:44:06 -0300

    mocking the new sudoku

--- client/src/components/jogar/SudokuRepresentation.tsx
+++ client/src/components/jogar/SudokuRepresentation.tsx
@@ -1,74 +1,54 @@
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import SudokuCell from "./SudokuCell";
-import { SudokuTable, getGame, submitGame, sudokuValue, verificaJogo } from "../../sudoku-game-API/sudokuAPI";
+import { SudokuFaces, SudokuTable, sudokuValue, verificaJogo } from "../../sudoku-game-API/sudokuAPI";
+import newGame from "../../sudoku-game-API/newGame.json";
+import SudokuFace from "./SudokuFace";
 
 export default function SudokuRepresentation({ id }: { id: number }) {
-  const navigate = useNavigate();
-
-  const [ state, setState ] = useState(false);
   const [ tabela, setTabela ] = useState<SudokuTable>();
-  const [ date ] = useState(new Date());
 
   useEffect(() => {
-    setState(true);
-
-    getGame(id)
-      .then(game => {
-        setState(false);
-
-        if (game === null) {
-          alert(`A tabela ${id} não existe`);
-          return;
-        }
-
-        if (game === undefined) {
-          alert("Não foi possível se conectar ao servidor");
-          return;
-        }
-
-        setTabela(game);
-      });
+    setTabela(generateTable());
   }, []);
 
-  function atualizaValor(posicao: number, newValue: sudokuValue | undefined) {
-    let novaTabela = (tabela as SudokuTable).map(cell => ({...cell}));
+  function generateTable(): SudokuTable {
+    const faces: Record<string, { valor: sudokuValue | undefined, isLocked: boolean }[][]> = {}
 
-    novaTabela[posicao].valor = newValue;
+    const createArray = () => new Array(4).fill(null)
+      .map(() => new Array(4).fill({ valor: undefined, isLocked: false }));
+
+    Object.keys(newGame).forEach(key => {
+      const newArray: { valor: sudokuValue | undefined, isLocked: boolean }[][] = createArray();
+
+      newGame[key as SudokuFaces].forEach(lockedCell => {
+        newArray[lockedCell.y][lockedCell.x] = {
+          valor: lockedCell.valor as sudokuValue,
+          isLocked: true
+        };
+      });
+      
+      faces[key as SudokuFaces] = newArray;
+    });
+
+    return faces as SudokuTable;
+  }
+
+  function atualizaValor(face: SudokuFaces, x: number, y: number, newValue: sudokuValue | undefined) {
+    const novaTabela = {...tabela as SudokuTable};
+
+    const novasCelulas = novaTabela[face].map(line => line.map(cell => ({...cell})));
+
+    novasCelulas[y][x].valor = newValue;
+
+    novaTabela[face] = novasCelulas;
 
     setTabela(novaTabela);
   }
 
   async function handleSubmit() {
-    const finishedTime = new Date().getTime();
-
-    if (!verificaJogo(tabela as SudokuTable)) {
-      return;
+    if (verificaJogo(tabela as SudokuTable)) {
+      console.log("parabéns");
     }
-
-    const initialTime = date.getTime();
-
-    submitGame(id, finishedTime - initialTime)
-      .then(id => {
-        if (id === undefined) {
-          alert("Não foi possível se conectar ao servidor");
-          return;
-        }
-
-        if (id === null) {
-          alert("Jogo não existe");
-          return;
-        }
-
-        alert("Jogo enviado com sucesso");
-      });
   }
-
-  if (state) return (
-    <div className="sudoku_rapper loading">
-      <h1>Carregando</h1>
-    </div>
-  )
 
   if (tabela === undefined) return (
     <div className="sudoku_rapper not-found">
@@ -79,19 +59,16 @@ export default function SudokuRepresentation({ id }: { id: number }) {
   return (
     <div className="sudoku_rapper">
       <div className="sudoku">
-        {tabela.map((cell, i) =>
-          <SudokuCell
-            key={i}
-            valor={cell.valor}
-            posicao={i}
+        {Object.entries(tabela).map(([ face, cells ]) =>
+          <SudokuFace
+            face={face as SudokuFaces}
+            cells={cells}
             atualizaValor={atualizaValor}
-            trancada={cell.isLocked}
           />
         )}
       </div>
       <div className="buttons">
-        <button onClick={handleSubmit}>Enviar</button>
-        <button onClick={() => navigate("/ranking/" + id)}>Ranking</button>
+        <button onClick={handleSubmit}>Checar</button>
       </div>
     </div>
   )
